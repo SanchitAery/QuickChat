a7ba14d8b52d235d20fed2e6f06153c53e308acb
Sanchit <sharmasanchit@seasiainfotech.com>
2019-11-22 12:22:07 +0530

    Code commited

---
+++ SSLibChat/SSLibChat.h
@@ -0,0 +1,19 @@
+//
+//  SSLibChat.h
+//  SSLibChat
+//
+//  Created by Sanchit Sharma on 11/22/19.
+//  Copyright © 2019 Sanchit Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSLibChat.
+FOUNDATION_EXPORT double SSLibChatVersionNumber;
+
+//! Project version string for SSLibChat.
+FOUNDATION_EXPORT const unsigned char SSLibChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSLibChat/PublicHeader.h>
+
+
